28dab4e6d60e30607ec8fdbc69097509d7ae863e
haikalhndrwn <haikalhendrawan88888@gmail.com>
2024-06-12 20:04:22 +0700

    adjust git ignore for build file

--- src/model/employee.model.ts
+++ src/model/employee.model.ts
@@ -85,7 +85,7 @@ interface EmpDataSql {
   MasaKerjaSeluruhnyaBln: number;
   HP: string;
   Email: string;
-}
+};
 
 
 class Employee{
@@ -189,7 +189,46 @@ class Employee{
                   VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40) 
                   RETURNING *`;
       const values = [
-        No, NIPFormatBaru, NIP, NamaGelar, GelarDepan, Nama, GelarBelakang, NamaTerakhir, Sex, BMI, KetBMI, Pangkat, Gol, GolTMT, Eselon, EselonTMT, TMTJabatan, TempatLahir, TanggalLahir, Usia, PendidikanTerakhir, PendidikanTerakhirShrt, Agama, Jabatan, KodeUnit, UnitEselonII, UnitEselonIII, UnitEselonIV, UnitKerja, UnitSulitTrs, RemoteZone, Status, AktifYN, UnitPertama, MasaKerjaGolonganThn, MasaKerjaGolonganBln, MasaKerjaSeluruhnyaThn, MasaKerjaSeluruhnyaBln, HP, Email
+        No, 
+        NIPFormatBaru, 
+        NIP, 
+        NamaGelar, 
+        GelarDepan, 
+        Nama, 
+        GelarBelakang, 
+        NamaTerakhir, 
+        Sex, 
+        BMI, 
+        KetBMI, 
+        Pangkat, 
+        Gol, 
+        GolTMT, 
+        Eselon, 
+        EselonTMT, 
+        TMTJabatan, 
+        TempatLahir, 
+        TanggalLahir, 
+        Usia, 
+        PendidikanTerakhir, 
+        PendidikanTerakhirShrt, 
+        Agama, 
+        Jabatan, 
+        KodeUnit, 
+        UnitEselonII, 
+        UnitEselonIII, 
+        UnitEselonIV, 
+        UnitKerja, 
+        UnitSulitTrs, 
+        RemoteZone, 
+        Status, 
+        AktifYN, 
+        UnitPertama, 
+        MasaKerjaGolonganThn, 
+        MasaKerjaGolonganBln, 
+        MasaKerjaSeluruhnyaThn, 
+        MasaKerjaSeluruhnyaBln, 
+        HP, 
+        Email
       ];
 
       const result = await client.query(q, values);
